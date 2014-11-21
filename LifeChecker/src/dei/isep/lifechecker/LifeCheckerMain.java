42935cd1a4aaf1eb0eff96f5a5d92411131f095e
1100570DiogoLeite <1100570@isep.ipp.pt>
2014-11-21 21:17:18 +0000

    Update ao código
    Alterações minimas

--- LifeChecker/src/dei/isep/lifechecker/LifeCheckerMain.java
+++ LifeChecker/src/dei/isep/lifechecker/LifeCheckerMain.java
@@ -10,6 +10,7 @@ public class LifeCheckerMain extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_life_checker_main);
 	}
 }
